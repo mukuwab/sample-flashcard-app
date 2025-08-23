b435cb628544174226ba02f77b5b133f7bbcf457
mukuwab <mukuwabaffoe@gmail.com>
2025-08-23 00:55:30 -0500

    t this is a commit this is a commit this is a commit this is a commit
    +
    this is a commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Flashcard from './Flashcard';
+
+//Need to store the SAMPLE_FLASHCARDS in a state variable
 
 function App() {
+  //This will be the main component for the flashcard app
+  //it will contain all of the other sub-components
+
+  const[flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  //function is setFlashcards
+  //initial value (default value) is SAMPLE_FLASHCARDS
+  //flashcards is the current value of the state variable
+
   return (
-    <div>
-      <h1>Hello World</h1>
-      <h2>Welcome to Flashcard App!!!!</h2>
-    </div>
+    <Flashcard flashcards={flashcards} />
   );
 }
+
+//sample questions that help render flashcards
+const SAMPLE_FLASHCARDS = [
+{
+  id: 1,
+  question: "What is 2 + 2?",
+  answer: "4",
+  options: ["2", "3", "4", "5"]
+
+},
+{
+  id: 2,
+  question: 'Question 2',
+  answer: "4",
+  options: [
+    'answer1',
+    'answer2',
+    'answer3',
+    'answer4'
+  ]
+
+}
+]
 
 export default App;
---
+++ src/FlashcardList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function FlashcardList() {
+    return(
+        <div>
+
+
+
+
+        </div>
+    )
+}
