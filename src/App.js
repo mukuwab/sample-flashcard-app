7785e50cd73b264223442fb1a51b0e2f8e2f5fb6
mukuwab <mukuwabaffoe@gmail.com>
2025-08-23 22:44:45 -0500

    API added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 //Need to store the SAMPLE_FLASHCARDS in a state variable
-import React, {useState} from 'react';
+import React, {useState,useEffect, use} from 'react';
 
 import FlashcardList from './FlashcardList'; //import Flashcard component
 //retrieve the Flashcard component from the Flashcard.js file
@@ -15,6 +15,16 @@ import FlashcardList from './FlashcardList'; //import Flashcard component
 
 import './App.css'; //import the CSS file for styling
 
+/*For the API go to: https://opentdb.com/api_config.php*/
+  /*10 questions, any for all others*/
+  /*copy the URL and use fetch to get the data from the API*/
+  /* you can paste the URL in the browser to see the data*/
+  /*to call the API, use library like axios*/
+  import axios from "axios";
+  /* need to add useEffect to make the API call as soon as the page loads*/
+  /* Add it to this: import React, {useState} from 'react'; */
+  /*Becomes import React, {useState,useEffect} from 'react';*/ 
+
 
 function App() {
   //This will be the main component for the flashcard app
@@ -24,6 +34,32 @@ function App() {
   //function is setFlashcards
   //initial value (default value) is SAMPLE_FLASHCARDS
   //flashcards is the current value of the state variable
+
+  useEffect(() => {
+    
+    axios.get("https://opentdb.com/api.php?amount=10")
+      .then(res => console.log(res.data))
+      .catch(err => console.error(err));
+    //axios
+      //.get('https://opentdb.com/api.php?amount=10')
+      //make a GET request to the API
+      //=10 means we want 10 questions, amount is set to 10
+      //.then(res => {
+        //console.log(res.data);
+        //log the data to the console to see what it looks like
+        //res is the response from the API
+        //res.data is the data we want
+        //we will need to format the data to match our flashcard format
+        //the data from the API is in a different format than our flashcard format
+        //we will need to map the data to our flashcard format
+        //we will use the map function to do this
+        //map function will loop through each item in the array and return a new array
+        //with the formatted data
+
+
+      
+
+  }, []); //empty dependency array means this will only run once when the component mounts
 
   return (
     <FlashcardList flashcards={flashcards} />//pass flashcards as a prop to Flashcard component
